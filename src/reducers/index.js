10a8488974a7d0b93532f82efba1203ca914ec28
Reggie <ryoboua@gmail.com>
2018-10-11 21:42:39 -0400

    Update src/reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,45 @@
 import { combineReducers } from 'redux'
 
-const APIPrefix = "http://api.openweathermap.org/data/2.5/forecast/daily?q="
-const APIKey = "e088f9ea8ecf5826cbe251b88a835822";
-
-function countrySelectReducer(state = 'CA', action) {
-    if (action.type === 'COUNTRY_SELECT'){
-        return action.country
+function countrySelectReducer(state = 'CA', { type, country }) {
+    if (type === 'COUNTRY_SELECT'){
+        return country
     } else {
         return state
     }
 }
 
-function unitSelectReducer( state = 'metric', action) {
-    if (action.type === 'SET_UNIT_FORMAT'){
-        return action.units
+function unitSelectReducer( state = 'metric', { type, units }) {
+    if (type === 'SET_UNIT_FORMAT'){
+        return units
     } else {
         return state
     }
 }
 
-function catchWeatherDataReducer( state = null, action) {
-    if (action.type === 'CATCH_WEATHER_DATA'){
-        return action.weatherData
+function catchWeatherData( state = null, { type, weatherDataArr }) {
+    if (type === 'CATCH_WEATHER_DATA') {
+        if (weatherDataArr[0].cod === '404') {
+            alert(weatherDataArr[0].message)
+            return state
+        } else {
+            return weatherDataArr
+        }
     } else {
         return state
     }
 }
+
+function cityReducer( state = '', { type, city } ) {
+    if(type === 'HANDLE_CITY_CHANGE') {
+        return city
+    } else {
+        return state
+    }
+}
+
 export default combineReducers({
     country: countrySelectReducer,
-    weatherData: catchWeatherDataReducer,
-    units: unitSelectReducer
+    weatherDataArr: catchWeatherData,
+    units: unitSelectReducer,
+    city: cityReducer,
 })
