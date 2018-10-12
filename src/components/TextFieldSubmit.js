db0bf22b3f8af8a5ad3649ae6f2251cfc6c4ca41
Reggie <ryoboua@gmail.com>
2018-10-11 21:42:26 -0400

    Update src/components/TextFieldSubmit.js

--- src/components/TextFieldSubmit.js
+++ src/components/TextFieldSubmit.js
@@ -1,38 +1,29 @@
 import React, { Component } from 'react';
+import store from '../store'
+import { handleSubmit } from '../helpers'
+
+import { handleCityChange } from '../actions/index'
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import TextField from '@material-ui/core/TextField'
 
-export default class TextFieldSubmit extends Component {
-    constructor(props){
-        super(props)
 
-        this.state = {
-            value: ''
-        }
-        this.onChange = this.onChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+export default class TextFieldSubmit extends Component { 
 
     onChange(e) {
-        this.setState({
-            value: e.target.value
-        })
-    }
-
-    handleSubmit() {
-        this.props.onSubmit(this.state.value)
+        store.dispatch(handleCityChange(e.target.value))
     }
 
     render() {
+        const { city } = store.getState()
         return (
             <div style={{display: 'inline-block'}} >
                 <TextField
-                        value={this.state.value}
+                        value={city}
                         onChange={this.onChange}
                         style={{margin: '0px 15px'}}
                         helperText="Select a city in the selected country"
                 />
-                <Button variant="raised" color="primary" onClick={this.handleSubmit}>
+                <Button variant="raised" color="primary" onClick={handleSubmit}>
                         Search
                 </Button>
             </div>
