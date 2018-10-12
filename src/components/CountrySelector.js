7cbc8076ba16f8b683675bb310cb93e10b9625ad
Reggie <ryoboua@gmail.com>
2018-10-11 21:42:21 -0400

    Update src/components/CountrySelector.js

--- src/components/CountrySelector.js
+++ src/components/CountrySelector.js
@@ -1,28 +1,26 @@
 
-import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
+import React, { Component } from 'react'
+import { setCountry } from '../actions/index'
+import store from '../store'
 import countryList from '../country_list'
 import { NativeSelect } from '@material-ui/core';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 
 export default class CountrySelector extends Component {
-    constructor(props){
-        super(props)
-        this.handleCountrySelect = this.handleCountrySelect.bind(this)
-    }
 
-    handleCountrySelect(event){
-        this.props.handleCountrySelect(event.target.value)
+    handleChange = e => {
+        store.dispatch(setCountry(e.target.value))
     }
 
     render(){
+        const { country } = store.getState()
         return (
             <FormControl>
                 <NativeSelect
                     id="select-country"
-                    value={this.props.selectedCountry}
-                    onChange={this.handleCountrySelect}
+                    value={country}
+                    onChange={this.handleChange}
                     >
                     { countryList.map( country => <option key={country.code} value={country.code}>{country.name}</option>) }
                 </NativeSelect>
