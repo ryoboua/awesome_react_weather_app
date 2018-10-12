90ed0a336cd0f8d7649e43642a7754587275d86d
Reggie <ryoboua@gmail.com>
2018-10-11 21:42:46 -0400

    Update src/store.js

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from './reducers/index'
+
+export default createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
